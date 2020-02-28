8109266f2ad6ee773d52b7167ff733fc0e43df2a
Edward Horsey <edwardhorsey@me.com>
2020-02-28 22:55:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,76 @@
 # My Code here....
+
+
+def map_to_negativize(source_array)
+index=0
+while index < source_array.count do
+      source_array[index] = source_array[index] * -1 
+      p source_array[index]
+      index += 1
+    end
+return source_array
+end
+
+def map_to_no_change(source_array)
+# index=0
+# while index < source_array.acount do
+#   source_array[index]
+#   index+=1
+# end
+return source_array
+end
+
+def map_to_double(source_array)
+    index=0
+    while index < source_array.count do
+          source_array[index] = source_array[index] * 2
+          p source_array[index]
+          index += 1
+        end
+    return source_array
+end
+
+def map_to_square(source_array)
+    index=0
+    while index < source_array.count do
+          source_array[index] = source_array[index] ** 2 
+          p source_array[index]
+          index += 1
+        end
+    return source_array
+end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+total=0
+    while starting_point<source_array.count do
+    total += source_array[starting_point]
+    starting_point+=1
+    end
+return total
+end
+
+
+def reduce_to_all_true(source_array)
+index=0
+while index<source_array.count do
+    if source_array[index] == !true
+        return false
+    end
+index+=1
+end
+true
+end
+
+def reduce_to_any_true(source_array)
+    index=0
+    while index<source_array.count do
+        if source_array[index] == true
+            return true
+        end
+    index+=1
+    end
+false
+end
+
+
